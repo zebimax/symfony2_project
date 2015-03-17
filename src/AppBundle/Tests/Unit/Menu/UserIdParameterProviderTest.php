446d6166de49358f56deb417979053da5d7bc56e
Maksym Perepelytsya <mperepelytsya@magecore.com>
2015-03-17 13:42:42 +0200

    OOT-160: Academic project on Symfony standard   - UserIdParameterProviderTest

---
+++ src/AppBundle/Tests/Unit/Menu/UserIdParameterProviderTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Tests\Unit\Menu;
+
+use AppBundle\Entity\Role;
+use AppBundle\Menu\Route\UserIdParameterProvider;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+class UserIdParameterProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetRouteParameters()
+    {
+        $storage = new TokenStorage();
+        $token = new UsernamePasswordToken('admin', null, 'main', [Role::ADMINISTRATOR]);
+
+        $user = $this
+            ->getMockBuilder('AppBundle\Entity\User')
+            ->getMock();
+        $user->expects($this->once())->method('getId');
+
+        $token->setUser($user);
+        $storage->setToken($token);
+        $object = new UserIdParameterProvider($storage);
+        $this->assertArrayHasKey('id', $object->getRouteParameters());
+    }
+}
