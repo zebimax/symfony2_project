c2819d3a3b3c9338ecfdf61f495f5559d86bfca0
Maksym Perepelytsya <mperepelytsya@magecore.com>
2015-03-17 18:30:40 +0200

    OOT-160: Academic project on Symfony standard   - MainMenuItemFactoryTest   - MainMenuManager and AppBundle/Menu/MenuBuilder refactoring

--- src/AppBundle/Menu/MainMenuManager.php
+++ src/AppBundle/Menu/MainMenuManager.php
@@ -2,59 +2,36 @@
 
 namespace AppBundle\Menu;
 
-use Knp\Menu\ItemInterface;
-use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
-use Symfony\Component\Translation\TranslatorInterface;
-
 class MainMenuManager
 {
     private $menuItems = [];
-    private $authorizationChecker;
-    private $translator;
 
     /**
-     * @param AuthorizationChecker $authorizationChecker
-     * @param TranslatorInterface $translatorInterface
      * @param array $menuItems
      */
-    public function __construct(
-        AuthorizationChecker $authorizationChecker,
-        TranslatorInterface $translatorInterface,
-        array $menuItems = []
-    ) {
-        $this->menuItems = $menuItems;
-        $this->authorizationChecker = $authorizationChecker;
-        $this->translator = $translatorInterface;
+    public function __construct(array $menuItems = []) {
+        $this->setMenuItems($menuItems);
     }
 
     /**
-     * @param ItemInterface $menuBuilder
+     * @return MainMenuItemInterface[]
      */
-    public function manageMenuItems(ItemInterface $menuBuilder)
+    public function getMenuItems()
     {
-        $this->addItems($menuBuilder, $this->menuItems);
+        return $this->menuItems;
     }
 
     /**
-     * @param ItemInterface $menuBuilder
      * @param array $menuItems
      */
-    private function addItems(ItemInterface $menuBuilder, array $menuItems)
+    private function setMenuItems($menuItems)
     {
         foreach ($menuItems as $item) {
-            if ($item instanceof MainMenuItemInterface && $this->authorizationChecker->isGranted($item->getName())) {
-                $itemOptions = ['routeParameters' => $item->getRouteParameters()];
-
-                if ($route = $item->getRoute()) {
-                    $itemOptions['route'] = $route;
-                }
-
-                $menuBuilderItem = $menuBuilder->addChild(
-                    $this->translator->trans($item->getLabel()),
-                    $itemOptions
-                );
-                $this->addItems($menuBuilderItem, $item->getSubItems());
+            if ($item instanceof MainMenuItemInterface) {
+                $this->menuItems[] = $item;
             }
         }
     }
+
+
 }
--- src/AppBundle/Menu/MenuBuilder.php
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -2,28 +2,32 @@
 namespace AppBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
 use Symfony\Bundle\FrameworkBundle\Translation\Translator;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 
 class MenuBuilder
 {
     private $factory;
     private $translator;
+    private $authorizationChecker;
 
     /**
      * @param FactoryInterface $factory
      * @param Translator $translator
+     * @param AuthorizationChecker $authorizationChecker
      */
-    public function __construct(FactoryInterface $factory, Translator $translator)
+    public function __construct(FactoryInterface $factory, Translator $translator, AuthorizationChecker $authorizationChecker)
     {
         $this->factory = $factory;
         $this->translator = $translator;
+        $this->authorizationChecker = $authorizationChecker;
     }
 
     public function createMainMenu(MainMenuManager $mainMenuManager)
     {
         $menu = $this->factory->createItem('root');
-
-        $mainMenuManager->manageMenuItems($menu);
+        $this->addItems($menu, $mainMenuManager->getMenuItems());
         return $menu;
     }
 
@@ -32,5 +36,28 @@ class MenuBuilder
         $menu = $this->factory->createItem('root');
         $menu->addChild($this->translator->trans('logout_menu.logout'), array('route' => 'logout'));
         return $menu;
+    }
+
+    /**
+     * @param ItemInterface $menuBuilder
+     * @param MainMenuItemInterface[] $menuItems
+     */
+    private function addItems(ItemInterface $menuBuilder, array $menuItems)
+    {
+        foreach ($menuItems as $item) {
+            if ($this->authorizationChecker->isGranted($item->getName())) {
+                $itemOptions = ['routeParameters' => $item->getRouteParameters()];
+
+                if ($route = $item->getRoute()) {
+                    $itemOptions['route'] = $route;
+                }
+
+                $menuBuilderItem = $menuBuilder->addChild(
+                    $this->translator->trans($item->getLabel()),
+                    $itemOptions
+                );
+                $this->addItems($menuBuilderItem, $item->getSubItems());
+            }
+        }
     }
 }
